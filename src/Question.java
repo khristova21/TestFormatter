5b55270c7548eb749a5774670dd565a70be72899
khristova21 <42977135+khristova21@users.noreply.github.com>
2018-11-29 13:36:59 -0500

    Add files via upload

---
+++ src/Question.java
@@ -0,0 +1,22 @@
+
+public abstract class Question 
+{
+
+	private int number = 1;
+	private String text = "";
+	
+	//constructors
+	//public Question()
+	
+	public void getNumber()
+	{
+		number = number;
+	}
+	
+	public void getText()
+	{
+		text = text;
+	}
+	public abstract String getSolution();
+	
+}
